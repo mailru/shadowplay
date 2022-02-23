76fcf5618d942aa4b0cea6e6de67f6e4c4013ba7
Evgenii Lepikhin <e.lepikhin@corp.mail.ru>
2022-02-23 13:56:42 +0300

    fix: Expressions priorities

--- puppet_pp_lint/src/tool/expression.rs
+++ puppet_pp_lint/src/tool/expression.rs
@@ -81,22 +81,22 @@ pub fn priority<EXTRA>(expr: &puppet_lang::expression::Expression<EXTRA>) -> u32
         | puppet_lang::expression::ExpressionVariant::GtEq(_)
         | puppet_lang::expression::ExpressionVariant::Lt(_)
         | puppet_lang::expression::ExpressionVariant::LtEq(_) => 3,
-        puppet_lang::expression::ExpressionVariant::ShiftLeft(_)
-        | puppet_lang::expression::ExpressionVariant::ShiftRight(_) => 4,
-        puppet_lang::expression::ExpressionVariant::Plus(_)
-        | puppet_lang::expression::ExpressionVariant::Minus(_) => 5,
-        puppet_lang::expression::ExpressionVariant::Multiply(_)
-        | puppet_lang::expression::ExpressionVariant::Divide(_)
-        | puppet_lang::expression::ExpressionVariant::Modulo(_) => 6,
-        puppet_lang::expression::ExpressionVariant::ChainCall(_) => 7,
-        puppet_lang::expression::ExpressionVariant::MatchRegex(_)
-        | puppet_lang::expression::ExpressionVariant::NotMatchRegex(_)
-        | puppet_lang::expression::ExpressionVariant::MatchType(_)
-        | puppet_lang::expression::ExpressionVariant::NotMatchType(_) => 8,
-        puppet_lang::expression::ExpressionVariant::Not(_) => 9,
         puppet_lang::expression::ExpressionVariant::In(_)
         | puppet_lang::expression::ExpressionVariant::Selector(_)
+        | puppet_lang::expression::ExpressionVariant::ShiftLeft(_)
+        | puppet_lang::expression::ExpressionVariant::ShiftRight(_)
+        | puppet_lang::expression::ExpressionVariant::Plus(_)
+        | puppet_lang::expression::ExpressionVariant::Minus(_)
+        | puppet_lang::expression::ExpressionVariant::Multiply(_)
+        | puppet_lang::expression::ExpressionVariant::Divide(_)
+        | puppet_lang::expression::ExpressionVariant::Modulo(_)
+        | puppet_lang::expression::ExpressionVariant::ChainCall(_)
+        | puppet_lang::expression::ExpressionVariant::MatchRegex(_)
+        | puppet_lang::expression::ExpressionVariant::NotMatchRegex(_)
+        | puppet_lang::expression::ExpressionVariant::MatchType(_)
+        | puppet_lang::expression::ExpressionVariant::NotMatchType(_)
+        | puppet_lang::expression::ExpressionVariant::Not(_)
         | puppet_lang::expression::ExpressionVariant::FunctionCall(_)
-        | puppet_lang::expression::ExpressionVariant::Term(_) => 10,
+        | puppet_lang::expression::ExpressionVariant::Term(_) => 4,
     }
 }
